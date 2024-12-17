bf690cf7716fb534f0b01b7a9c4e76ada3f382cf
Raissa Queiroz <raifreelas@gmail.com>
2024-12-17 10:15:41 -0300

    fix: fixed configs

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,28 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react'
-
+import react from '@vitejs/plugin-react';
 import { VitePluginNode } from 'vite-plugin-node';
 
 export default defineConfig({
   server: {
-    port: 3001
+    port: 3001,
   },
   plugins: [
-    [react()],
-    
+    react(),
     VitePluginNode({
       adapter: 'express',
       appPath: './src/index.ts',
-      exportName: 'app',
-      tsCompiler: 'esbuild'
-    })
-  ]
+      exportName: 'app', 
+      tsCompiler: 'esbuild',
+    }),
+  ],
+  resolve: {
+    alias: {
+      https: 'rollup-plugin-node-builtins',
+    },
+  },
+  build: {
+    rollupOptions: {
+      external: ['https', 'http', 'zlib', 'stream'],
+    },
+  },
 });
